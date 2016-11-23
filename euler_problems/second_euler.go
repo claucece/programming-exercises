7b4f30411959e4d6c9a32fb27f807529e08774e2
Sofia Celi <sceli@thoughtworks.com>
2016-11-23 01:38:31 -0500

    adds test for second problem

--- euler_problems/second_euler.go
+++ euler_problems/second_euler.go
@@ -1,12 +1,17 @@
 package euler
 
 func fibonacci(n int) int {
-	switch n {
-	case 0:
-		return 0
-	case 1:
-		return 1
-	default:
-		return fibonacci(n-1) + fibonacci(n-2)
+	var result int
+	for result < n {
+		switch n {
+		case 0:
+			return 0
+		case 1:
+			return 1
+		default:
+			numbers := fibonacci(n-1) + fibonacci(n-2)
+			result += numbers
+		}
 	}
+	return result
 }
