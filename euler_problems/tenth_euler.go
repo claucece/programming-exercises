f1f6376a61b2805964422c5a3782d108fdde3405
Sofia Celi <sceli@thoughtworks.com>
2016-12-07 02:37:06 -0500

    solves tenth problem

--- euler_problems/tenth_euler.go
+++ euler_problems/tenth_euler.go
@@ -1,13 +1,49 @@
 package euler
 
-// still in development. Will probably be solvedby Sieve. Just trying to look
-// for a way to brute force it with small values.
-func findSumOfPrimes(n int) int {
+// TODO: altough sieve is fine, there is also Atkin.
+
+// a weird brute force one. It is only working until 25, of course.
+func findSumOfPrimesByBruteForce(n int) int {
 	var sum int
 	for i := 2; i < n; i++ {
-		if n%i == 0 {
-			sum += i
+		if i == 2 || i%2 != 0 {
+			if i == 3 || i%3 != 0 {
+				sum += i
+			}
 		}
 	}
 	return sum
+}
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func sumPrimes(primes []int) int {
+	var sum int
+	for _, i := range primes {
+		sum += i
+	}
+	return sum
+}
+
+// TODO: check the math of this
+func findPrimesBySieve(upperN int) int {
+	primes := []int{2}
+	notPrimes := []int{1}
+
+	for i := 3; i <= upperN; i += 2 {
+		for j := i * i; j <= upperN; j += 2 * i {
+			notPrimes = append(notPrimes, j)
+		}
+		if !(contains(notPrimes, i)) {
+			primes = append(primes, i)
+		}
+	}
+	return sumPrimes(primes)
 }
--- euler_problems/tenth_euler_test.go
+++ euler_problems/tenth_euler_test.go
@@ -7,5 +7,6 @@ import (
 func (s *EulerSuite) Test_FindSumOfPrimes(c *C) {
 	expected_result := 17
 
-	c.Assert(findSumOfPrimes(10), Equals, expected_result)
+	c.Assert(findSumOfPrimesByBruteForce(10), Equals, expected_result)
+	c.Assert(findPrimesBySieve(10), Equals, expected_result)
 }
