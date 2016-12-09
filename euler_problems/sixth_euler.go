48788c652853e462ee1126ef5b0ae9c854d0bccc
Sofia Celi <sceli@thoughtworks.com>
2016-12-09 00:52:54 -0500

    refactors sixth problem

--- euler_problems/sixth_euler.go
+++ euler_problems/sixth_euler.go
@@ -1,31 +1,30 @@
 package euler
 
-// brute force
-func sumSquares(n int) int {
-	var sum int
-	for n := 1; n <= 100; n++ {
-		sum += n * n
+const N = 100
+
+// by brute force
+func sumSquares() int {
+	sum := 0
+	for i := 1; i <= N; i++ {
+		sum += i * i
 	}
 	return sum
 }
 
-func squareSum(n int) int {
-	var square int
-	for n := 1; n <= 100; n++ {
-		square += n
+func squareSum() int {
+	square := 0
+	for i := 1; i <= N; i++ {
+		square += i
 	}
 	return square * square
 }
 
-func difference(n, x int) int {
-	return squareSum(n) - sumSquares(x)
+func difference() int {
+	return squareSum() - sumSquares()
 }
 
 // by Gauss formula and extended
-
-func calculateDifference() int {
-	const N = 100
-
+func calculateDifferenceOfSquares() int {
 	sum := N * (N + 1) / 2
 	square := (N * (N + 1) * (2*N + 1)) / 6
 	difference := sum*sum - square
--- euler_problems/sixth_euler_test.go
+++ euler_problems/sixth_euler_test.go
@@ -4,9 +4,9 @@ import (
 	. "gopkg.in/check.v1"
 )
 
-func (s *EulerSuite) TestFindDifference(c *C) {
+func (s *EulerSuite) Test_FindDifferenceOfSquares(c *C) {
 	expected_result := 25164150
 
-	c.Assert(difference(100, 100), Equals, expected_result)
-	c.Assert(calculateDifference(), Equals, expected_result)
+	c.Assert(difference(), Equals, expected_result)
+	c.Assert(calculateDifferenceOfSquares(), Equals, expected_result)
 }
