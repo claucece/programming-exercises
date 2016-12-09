2241d06c3b3c4ed05de642d52535c33b74724413
Sofia Celi <sceli@thoughtworks.com>
2016-12-09 02:21:31 -0500

    refactors ninth problem

--- euler_problems/ninth_euler.go
+++ euler_problems/ninth_euler.go
@@ -6,14 +6,12 @@ import (
 
 // by brute force. Traditional Pythagorean triplet
 func findAPithagoreanTriplet() (int, int, int) {
-	var a, b, c int
-	for c = 0; c <= 1000; c++ {
-		for b = 0; b < c; b++ {
-			for a = 0; a < b; a++ {
+	for c := 0; c <= 1000; c++ {
+		for b := 0; b < c; b++ {
+			for a := 0; a < b; a++ {
 				if a*a+b*b == c*c {
 					return a, b, c
 				}
-
 			}
 		}
 	}
@@ -22,15 +20,13 @@ func findAPithagoreanTriplet() (int, int, int) {
 
 // by brute force but also applying this condition: a < 1000/3,  and a < b < 1000/2
 func findAPithagoreanTripletByCondition() (int, int, int) {
-	var a, b, c int
 	s := 1000
-	for a = 1; a <= s/3; a++ {
-		for b = a; b < s/2; b++ {
-			c = s - a - b
+	for a := 1; a <= s/3; a++ {
+		for b := a; b < s/2; b++ {
+			c := s - a - b
 			if (a*a)+(b*b) == (c * c) {
 				return a, b, c
 			}
-
 		}
 	}
 	return 0, 0, 0
@@ -53,8 +49,7 @@ func gcd(a, b int) int {
 	}
 
 	for x%y != 0 {
-		var temp int
-		temp = x
+		temp := x
 		x = y
 		y = temp % x
 	}
@@ -62,14 +57,12 @@ func gcd(a, b int) int {
 }
 
 func findPithagoreanTripletByEuclid() (int, int, int) {
-	var a, b, c int
 	s := 1000
-	var m, k, n, d int
-	found := false
+	a, b, c, k := 0, 0, 0, 0
 
 	limit := int(math.Sqrt(float64(s) / 2.0))
 
-	for m = 2; m <= limit; m++ {
+	for m := 2; m <= limit; m++ {
 		if (s/2)%m == 0 {
 			if m%2 == 0 {
 				k = m + 1
@@ -78,19 +71,14 @@ func findPithagoreanTripletByEuclid() (int, int, int) {
 			}
 			for k < 2*m && k <= s/(2*m) {
 				if s/(2*m)%k == 0 && gcd(k, m) == 1 {
-					d = s / 2 / (k * m)
-					n = k - m
+					d := s / 2 / (k * m)
+					n := k - m
 					a = d * (m*m - n*n)
 					b = 2 * d * n * m
 					c = d * (m*m + n*n)
-					found = true
-					break
 				}
 				k += 2
 			}
-		}
-		if found {
-			break
 		}
 	}
 	return a, b, c
